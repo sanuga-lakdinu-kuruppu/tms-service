e4afb9fad2f472d08a2293c29dd35b9d337f1ae6
Sanuga Kuruppu <128165236+sanuga-lakdinu-kuruppu@users.noreply.github.com>
2026-02-14 09:00:35 +0530

    add update task by id endpoint added

--- src/task/controller/taskController.mjs
+++ src/task/controller/taskController.mjs
@@ -3,8 +3,12 @@ import { checkSchema, matchedData, validationResult } from "express-validator";
 import { RETURN } from "../../common/error/returnCodes.mjs";
 import { USER_TYPES } from "../../user/enum/userType.mjs";
 import { protectRoute } from "../../auth/middleware/authMiddleware.mjs";
-import { createTaskSchema } from "../schema/taskSchemas.mjs";
-import { createTask, getAllTasks } from "../service/taskService.mjs";
+import { createTaskSchema, updateTaskSchema } from "../schema/taskSchemas.mjs";
+import {
+  createTask,
+  getAllTasks,
+  updateTaskById,
+} from "../service/taskService.mjs";
 
 const router = Router();
 
@@ -78,6 +82,57 @@ router.get(
       }
     } catch (error) {
       console.log(`Error during task retriving process: ${error}`);
+      return response.status(500).send({
+        msg: "Oops! Something went wrong on our end. Please try again later.",
+      });
+    }
+  }
+);
+
+router.put(
+  "/v1/tasks/:taskId",
+  protectRoute([USER_TYPES.MEMBER]),
+  checkSchema(updateTaskSchema),
+  async (request, response) => {
+    try {
+      //request validation
+      const result = validationResult(request);
+      if (!result.isEmpty()) {
+        console.log(
+          `error occured during validation: ${result.errors[0].msg} :)`
+        );
+        return response.status(400).send({ error: result.errors[0].msg });
+      }
+      const data = matchedData(request);
+      const { user } = request;
+      const { taskId } = request.params;
+
+      //request processing
+      const { res, value } = await updateTaskById(user, data, taskId);
+      if (res === RETURN.SUCCESS) {
+        console.log(`task update process successfull :)`);
+        return response.status(200).send({
+          msg: "Task update successfully",
+          data: { task: value.task },
+        });
+      } else if (res === RETURN.NOT_FOUND) {
+        console.log(`task not found :)`);
+        return response.status(404).send({
+          msg: "Task not found",
+        });
+      } else if (res === RETURN.UNAUTHORIZED) {
+        console.log(`task not authorized :)`);
+        return response.status(401).send({
+          msg: "You are not authorized to update this task",
+        });
+      } else {
+        console.log(`Task update process unsuccessful :)`);
+        return response.status(500).send({
+          msg: "Oops! Something went wrong on our end. Please try again later.",
+        });
+      }
+    } catch (error) {
+      console.log(`Error during task update process: ${error}`);
       return response.status(500).send({
         msg: "Oops! Something went wrong on our end. Please try again later.",
       });
